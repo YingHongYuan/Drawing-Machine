69c76678b615453b862054add89f70ea892c8d3d
YingHongYuan <yinghong.yuan@sjsu.edu>
2020-03-23 07:16:52 -0700

    1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(100,80,160);
+  strokeWeight(5);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed){
+    line(mouseX,mouseY,pmouseX,pmouseY);
+    //array.push([mouseX,mouseY]);
+  }
+
+
+function keyTyped(){
+  if (key == 's'){
+    //save this images
+    saveCanvas('filename','png');
+  }
+    //line(array[i][0],array[i][1],array[i+1][0],array[i+1],[1]);
+  }
+  return false;
 }
